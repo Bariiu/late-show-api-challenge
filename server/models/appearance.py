e3858825ad89d1bff4feead55d81c7a897f447dd
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 00:34:55 +0300

    Defined the appearance model

---
+++ server/models/appearance.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.schema import UniqueConstraint
+from server.app import db
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    guest = relationship('Guest', back_populates='appearances')
+    episode = relationship('Episode', back_populates='appearances')
+
+    __table_args__ = (UniqueConstraint('guest_id', 'episode_id', name='_guest_episode_uc'),)
