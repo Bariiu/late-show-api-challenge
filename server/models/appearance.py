1b5ae80fab17895b9abbc8df84e97f9cefeb5b42
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 00:36:14 +0300

    Added rating

--- server/models/appearance.py
+++ server/models/appearance.py
@@ -15,3 +15,12 @@ class Appearance(db.Model):
     episode = relationship('Episode', back_populates='appearances')
 
     __table_args__ = (UniqueConstraint('guest_id', 'episode_id', name='_guest_episode_uc'),)
+
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        """Ensures the rating is between 1 and 5 (inclusive)"""
+        if not isinstance(rating, int):
+            raise ValueError("Rating must be an integer.")
+        if not (1 <= rating <= 5):
+            raise ValueError("Rating must be between 1 and 5.")
+        return rating
