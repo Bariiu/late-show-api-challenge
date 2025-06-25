17e44d79d5e5d6b13ccfc758f8693522f51b1ebe
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 16:54:18 +0300

    added GET actions in episode controller

---
+++ server/controllers/episode_controller.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
+from server.models.episode import Episode
+from server.app import db
+
+episode_bp = Blueprint("episodes", __name__)
+
+@episode_bp.route("/", methods=["GET"])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([{"id": e.id, "date": e.date.isoformat(), "number": e.number} for e in episodes])
+
+
+@episode_bp.route("/<int:id>", methods=["GET"])
+def get_episode(id):
+    episode = Episode.query.get_or_404(id)
+    return jsonify({
+        "id": episode.id,
+        "date": episode.date.isoformat(),
+        "number": episode.number,
+        "appearances": [
+            {
+                "id": a.id,
+                "guest": {
+                    "id": a.guest.id,
+                    "name": a.guest.name,
+                    "occupation": a.guest.occupation
+                },
+                "rating": a.rating
+            }
+            for a in episode.appearances
+        ]
+    })
+
