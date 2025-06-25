e5466ff229c71b105871ef2fe1906facf92b4395
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 16:26:59 +0300

    Registered blueprints

--- server/app.py
+++ server/app.py
@@ -15,3 +15,17 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+
+    from .controllers.guest_controller import guest_bp
+    from .controllers.episode_controller import episode_bp
+    from .controllers.appearance_controller import appearance_bp
+    from .controllers.auth_controller import auth_bp
+
+    app.register_blueprint(guest_bp, url_prefix="/guests")
+    app.register_blueprint(episode_bp, url_prefix="/episodes")
+    app.register_blueprint(appearance_bp, url_prefix="/appearances")
+    app.register_blueprint(auth_bp)
+
+    return app
+
+app = create_app()
