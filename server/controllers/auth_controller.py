f83b2735d3479ee2a0039d21b05c9865c32cfbd8
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 16:36:44 +0300

    implemented user registration

---
+++ server/controllers/auth_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify
+from server.app import db
+from server.models.user import User
+from flask_jwt_extended import create_access_token
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "Username and password required"}), 400
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "Username already exists"}), 409
+
+    user = User(username=username)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully"}), 201
