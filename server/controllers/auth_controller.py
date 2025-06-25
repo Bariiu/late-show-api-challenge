71bbc07162bd0283d2481a2b3436f94edc55d4cb
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-25 16:37:30 +0300

    added registration and login routes

--- server/controllers/auth_controller.py
+++ server/controllers/auth_controller.py
@@ -23,3 +23,17 @@ def register():
     db.session.commit()
 
     return jsonify({"message": "User registered successfully"}), 201
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.check_password(password):
+        return jsonify({"error": "Invalid credentials"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify(access_token=access_token), 200
